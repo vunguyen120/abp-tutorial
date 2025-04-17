39fe89e809f15de5082a757321c44110d3c49ee5
MoldBreaker <vunguyen10a8.nt@gmail.com>
2025-04-17 17:41:57 +0700

    first commit

---
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissions.cs
@@ -0,0 +1,26 @@
+namespace Acme.BookStore.Permissions;
+
+public static class BookStorePermissions
+{
+    public const string GroupName = "BookStore";
+
+    // other permissions...
+    // other permissions...
+
+    // *** ADDED a NEW NESTED CLASS ***
+    public static class Books
+    {
+        public const string Default = GroupName + ".Books";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
+
+    public static class Authors
+    {
+        public const string Default = GroupName + ".Authors";
+        public const string Create = Default + ".Create";
+        public const string Edit = Default + ".Edit";
+        public const string Delete = Default + ".Delete";
+    }
+}
---
+++ src/Acme.BookStore.Web/Menus/BookStoreMenuContributor.cs
@@ -0,0 +1,72 @@
+using System.Threading.Tasks;
+using Acme.BookStore.Localization;
+using Acme.BookStore.Permissions;
+using Acme.BookStore.MultiTenancy;
+using Volo.Abp.SettingManagement.Web.Navigation;
+using Volo.Abp.Authorization.Permissions;
+using Volo.Abp.Identity.Web.Navigation;
+using Volo.Abp.UI.Navigation;
+using Volo.Abp.TenantManagement.Web.Navigation;
+
+namespace Acme.BookStore.Web.Menus;
+
+public class BookStoreMenuContributor : IMenuContributor
+{
+    public async Task ConfigureMenuAsync(MenuConfigurationContext context)
+    {
+        if (context.Menu.Name == StandardMenus.Main)
+        {
+            await ConfigureMainMenuAsync(context);
+        }
+    }
+
+    private static Task ConfigureMainMenuAsync(MenuConfigurationContext context)
+    {
+        var l = context.GetLocalizer<BookStoreResource>();
+
+        //Home
+        context.Menu.AddItem(
+            new ApplicationMenuItem(
+                "BooksStore",
+                l["Menu:BookStore"],
+                icon: "fa fa-book"
+            ).AddItem(
+                new ApplicationMenuItem(
+                    "BooksStore.Books",
+                    l["Menu:Books"],
+                    url: "/Books"
+                ).RequirePermissions(BookStorePermissions.Books.Default)
+            ).AddItem( 
+                new ApplicationMenuItem(
+                    "BooksStore.Authors",
+                    l["Menu:Authors"],
+                    url: "/Authors"
+                ).RequirePermissions(BookStorePermissions.Authors.Default)
+            )
+        );
+
+
+        //Administration
+        var administration = context.Menu.GetAdministration();
+        administration.Order = 6;
+
+        //Administration->Identity
+        administration.SetSubItemOrder(IdentityMenuNames.GroupName, 1);
+    
+        if (MultiTenancyConsts.IsEnabled)
+        {
+            administration.SetSubItemOrder(TenantManagementMenuNames.GroupName, 1);
+        }
+        else
+        {
+            administration.TryRemoveMenuItem(TenantManagementMenuNames.GroupName);
+        }
+        
+        administration.SetSubItemOrder(SettingManagementMenuNames.GroupName, 3);
+
+        //Administration->Settings
+        administration.SetSubItemOrder(SettingManagementMenuNames.GroupName, 7);
+        
+        return Task.CompletedTask;
+    }
+}
